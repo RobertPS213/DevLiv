533300f7a67153db0c906ecb5b35a7d66a58fbb8
Robert Pereira Santos <robert21.12pereira@gmail.com>
2026-07-27 12:50:03 -0300

    Initial commit: estrutura base do projeto

---
+++ src/main/java/com/devlib/devlib/DevlibApplication.java
@@ -0,0 +1,13 @@
+package com.devlib.devlib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevlibApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevlibApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devlib/devlib/DevlibApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devlib.devlib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevlibApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
