0364664a9d264ce5583ddc1d0b16bc02bd570fe0
Robert Pereira Santos <robert21.12pereira@gmail.com>
2026-07-28 19:27:24 -0300

    feat: CRUD completo do Livro

--- src/main/java/com/devlib/devlib/controller/LivroController.java
+++ src/main/java/com/devlib/devlib/controller/LivroController.java
@@ -1,15 +1,75 @@
 package com.devlib.devlib.controller;
 
+import java.net.URI;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.devlib.devlib.entites.Livro;
 import com.devlib.devlib.services.LivroService;
 
 @RestController
-@RequestMapping(value = "/livro")
+@RequestMapping(value = "/livros")
 public class LivroController {
 
 	@Autowired
 	private LivroService service;
+	
+	@GetMapping
+	public ResponseEntity<List<Livro>> findAll(){
+		List<Livro> livros = service.findAll();
+		return ResponseEntity.ok().body(livros);
+	}
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Livro> findById(@PathVariable Long id){
+		Livro livro = service.findById(id);
+		return ResponseEntity.ok().body(livro);
+	}
+	@PostMapping
+	public ResponseEntity<Livro> insert(@RequestBody Livro livro){
+		livro = service.insert(livro);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(livro.getId()).toUri();
+		return ResponseEntity.created(uri).body(livro);
+	}
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<Livro> update(@PathVariable Long id, @RequestBody Livro entity){
+		Livro livro = service.update(id, entity);
+		return ResponseEntity.ok().body(livro);
+	}
+	@PostMapping(value = "/{livroId}/autores/{autorId}")
+	public ResponseEntity<Livro> adicionarAutores(@PathVariable Long livroId, @PathVariable Long autorId){
+		Livro livro = service.adicionarAutor(livroId, autorId);
+		return ResponseEntity.ok().body(livro);
+	}
+	@DeleteMapping(value = "/{livroId}/autores/{autorId}")
+	public ResponseEntity<Livro> removerAutores(@PathVariable Long livroId, @PathVariable Long autorId){
+		Livro livro = service.removerAutor(livroId, autorId);
+		return ResponseEntity.ok().body(livro);
+	}
+	@PostMapping(value = "/{livroId}/categorias/{categoriaId}")
+	public ResponseEntity<Livro> adicionarCategoria(@PathVariable Long livroId, @PathVariable Long categoriaId){
+		Livro livro = service.adicionarCategoria(livroId, categoriaId);
+		return ResponseEntity.ok().body(livro);
+	}
+	@DeleteMapping(value = "/{livroId}/categorias/{categoriaId}")
+	public ResponseEntity<Livro> removeCategoria(@PathVariable Long livroId, @PathVariable Long categoriaId){
+		Livro livro = service.removerCategoria(livroId, categoriaId);
+		return ResponseEntity.ok().body(livro);
+	}
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id){
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
 }
+
--- src/main/java/com/devlib/devlib/services/LivroService.java
+++ src/main/java/com/devlib/devlib/services/LivroService.java
@@ -1,8 +1,16 @@
 package com.devlib.devlib.services;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.devlib.devlib.entites.Autor;
+import com.devlib.devlib.entites.Categoria;
+import com.devlib.devlib.entites.Livro;
+import com.devlib.devlib.repositories.AutorRepository;
+import com.devlib.devlib.repositories.CategoriaRepository;
 import com.devlib.devlib.repositories.LivroRepository;
 
 @Service
@@ -10,4 +18,62 @@ public class LivroService {
 	
 	@Autowired
 	private LivroRepository repository;
+	
+	@Autowired
+	private AutorRepository autorRepository;
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
+	public List<Livro> findAll(){
+		return repository.findAll();
+	}
+	public Livro findById(Long id) {
+		Optional<Livro> livro = repository.findById(id);
+		return livro.get();
+	}
+	public Livro insert(Livro livro) {
+		return repository.save(livro);
+	}
+	public Livro update(Long id, Livro entity) {
+		Livro livro = repository.getReferenceById(id);
+		updateData(livro, entity);
+		return repository.save(livro);
+	}
+	public void updateData(Livro livro, Livro entity) {
+		livro.setTitulo(entity.getTitulo());
+		livro.setIsbn(entity.getIsbn());
+		livro.setAnoPublicacao(entity.getAnoPublicacao());
+		livro.setNumeroPaginas(entity.getNumeroPaginas());
+		livro.setEditora(entity.getEditora());
+		livro.setEstante(entity.getEstante());
+	}
+	public Livro adicionarAutor(Long livroId, Long autorId) {
+		Livro livro = repository.getReferenceById(livroId);
+		Autor autor = autorRepository.getReferenceById(autorId);
+		livro.getAutores().add(autor);
+		repository.save(livro);
+		return livro;
+	}
+	public Livro removerAutor(Long livroId, Long autorId) {
+		Livro livro = repository.getReferenceById(livroId);
+		Autor autor = autorRepository.getReferenceById(autorId);
+		livro.getAutores().remove(autor);
+		return repository.save(livro);
+	}
+	public Livro adicionarCategoria(Long livroId, Long categoriaId) {
+		Livro livro = repository.getReferenceById(livroId);
+		Categoria categoria = categoriaRepository.getReferenceById(categoriaId);
+		livro.getCategorias().add(categoria);
+		return repository.save(livro);
+	}
+	public Livro removerCategoria(Long livroId, Long categoriaId) {
+		Livro livro = repository.getReferenceById(livroId);
+		Categoria categoria = categoriaRepository.getReferenceById(categoriaId);
+		livro.getCategorias().remove(categoria);
+		return repository.save(livro);
+	}
+	public void delete(Long id) {
+		repository.deleteById(id);
+	}
 }
